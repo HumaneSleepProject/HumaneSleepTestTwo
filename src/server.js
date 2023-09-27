9fcb2b380f905d451f83ee6ec819d70350c9c6b4
HumaneSleepProject <144190170+HumaneSleepProject@users.noreply.github.com>
2023-09-26 22:18:16 -0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import sirv from "sirv";
+import express from "express";
+import compression from "compression";
+import * as sapper from "@sapper/server";
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === "development";
+
+express()
+  .use(
+    process.env.BASE_NAME || '/smui-sapper-rollup-template',
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log("error", err);
+  });
